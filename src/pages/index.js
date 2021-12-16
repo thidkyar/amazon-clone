0d47509455b39c71184e7f61afde7c34ffc5bb8a
Taranvir Hidkyar <thidkyar@gmail.com>
2021-12-16 14:34:13 -0500

    create and style product feed and product components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,31 @@ import Banner from "../components/Banner";
 import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
-  return (
-    <div className="bg-gray-100">
-      <Head>
-        <title>Amazon 2.0</title>
-      </Head>
-    <Header />
+export default function Home({ products }) {
+    return (
+        <div className="bg-gray-100">
+            <Head>
+                <title>Amazon 2.0</title>
+            </Head>
+            <Header />
 
-    <main className="max-w-screen-2xl mx-auto">
-      <Banner />
-      <ProductFeed />
-    </main>
-    </div>
-  );
+            <main className="max-w-screen-2xl mx-auto">
+                <Banner />
+                <ProductFeed products={products}/>
+            </main>
+        </div>
+    );
+}
+
+/* tells next js it is no longer a static page. Render page then deliver - otherwise page will show without products*/
+export async function getServerSideProps(context) {
+    const products = await fetch("https://fakestoreapi.com/products").then(
+        (res) => res.json()
+    );
+
+    return {
+        props: {
+            products,
+        },
+    };
 }
