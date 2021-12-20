97eb399aec996a2e354034f9f165856e63151e1f
Taranvir Hidkyar <thidkyar@gmail.com>
2021-12-19 22:12:29 -0500

    add discord auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,20 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
 
 /* if you get redirect URI error -> go to google console -> your project -> credentials -> update URLs */
 export default NextAuth({
-  // Configure one or more authentication providers
-  // secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    // ...add more providers here
-  ],
-})
+    // Configure one or more authentication providers
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_ID,
+            clientSecret: process.env.DISCORD_SECRET,
+        }),
+        // ...add more providers here
+    ],
+});
