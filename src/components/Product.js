fb20d4b3019d96f50292dd3de5436f7a980d29fc
Taranvir Hidkyar <thidkyar@gmail.com>
2021-12-21 00:11:54 -0500

    change toast view

--- src/components/Product.js
+++ src/components/Product.js
@@ -24,6 +24,7 @@ function Product({ id, title, price, description, category, image }) {
             pauseOnHover: false,
             autoClose: 3000,
             theme: "colored",
+            position: "bottom-right",
         });
 
     const addItemToBasket = () => {
